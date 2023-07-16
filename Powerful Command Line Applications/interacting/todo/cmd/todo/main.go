1ece1fb0908be1141b17215b6feb8206ecaba8dc
srini <ksrinivasrao531@gmail.com>
2023-07-16 17:57:28 +0530

    todo list cli is complete

--- Powerful Command Line Applications/interacting/todo/cmd/todo/main.go
+++ Powerful Command Line Applications/interacting/todo/cmd/todo/main.go
@@ -1,24 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/hellskater/todo"
 )
 
-const todoFileName = ".todo.json"
+var todoFileName = ".todo.json"
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for The Pragmatic Bookshelf \n ",
+			os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2020 \n ")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
 
-	task := flag.String("task", "", "Task to be added")
+		flag.PrintDefaults()
+	}
+
+	add := flag.Bool("add", false, "Task to be added")
 
 	list := flag.Bool("list", false, "List all tasks")
 
 	complete := flag.Int("complete", 0, "Item to be completed")
 
 	flag.Parse()
+
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
 
 	l := &todo.List{}
 
@@ -29,11 +45,7 @@ func main() {
 
 	switch {
 	case *list:
-		for _, item := range *l {
-			if !item.Done {
-				fmt.Println(item.Task)
-			}
-		}
+		fmt.Print(l)
 
 	case *complete > 0:
 		if err := l.Complete(*complete); err != nil {
@@ -46,8 +58,14 @@ func main() {
 			os.Exit(1)
 		}
 
-	case *task != "":
-		l.Add(*task)
+	case *add:
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		l.Add(t)
 
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -59,4 +77,24 @@ func main() {
 		os.Exit(1)
 
 	}
+}
+
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+
+	s.Scan()
+
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("task cannot be blank")
+	}
+
+	return s.Text(), nil
 }
